80d48d0f4d961bac4c8d4966ff114158d02238a4
Kris Kozak <kriskozak@hatchbed.com>
2019-06-01 13:59:01 -0500

    Add support for E-series robot
    (cherry picked from: 4a608c9e1e2a)

--- include/ur_modern_driver/ros/action_server.h
+++ include/ur_modern_driver/ros/action_server.h
@@ -1,23 +1,3 @@
-/*
- * Copyright 2017, 2018 Jarek Potiuk (low bandwidth trajectory follower)
- *
- * Copyright 2017, 2018 Simon Rasmussen (refactor)
- *
- * Copyright 2015, 2016 Thomas Timm Andersen (original version)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #pragma once
 
 #include <actionlib/server/action_server.h>
@@ -90,4 +70,5 @@ public:
   virtual bool consume(RTState_V1_8& state);
   virtual bool consume(RTState_V3_0__1& state);
   virtual bool consume(RTState_V3_2__3& state);
+  virtual bool consume(RTState_V3_5__5_1& state);
 };
